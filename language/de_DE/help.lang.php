12899b229f76218cf365075809d1613ecd7c2480
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-22 12:20:11 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/help.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Kopieren Sie nach der Registrierung die erstellten Anmeldedaten und fügen Sie sie auf dieser Einrichtungsseite ein';
+$lang['Go to <a href="%s" target="_blank">%s</a> > Azure Active Directory > App registrations > Create a tenant'] = 'Gehen Sie zu <a href="%s" target="_blank">%s</a> > Azure Active Directory > App-Registrierungen > Erstellen eines Mandanten';
+$lang['In the tenant configuration, go to > Certificates & secrets. Create a new client secret. (= Application Secret)'] = 'Gehen Sie in der Mandantenkonfiguration auf > Zertifikate & Geheimnisse. Erstellen Sie ein neues Client Secret. (= Anwendungsgeheimnis)';
+$lang['And in > Azure Active Directory > Properties, get the Tenant ID'] = 'Und in > Azure Active Directory > Eigenschaften, erhalten Sie die Tenant ID';
+$lang['Fill the name and redirect URI :  https://your_piwigo/plugins/m365connect/auth.php'] = 'Füllen Sie den Namen und die Umleitungs-URI aus: https://your_piwigo/plugins/m365connect/auth.php';
+$lang['Get the Application (client) ID (= Application Key)'] = 'Abrufen der Anwendungs-(Client-)ID (= Anwendungsschlüssel)';
---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Tenant ID'] = 'Mieter-ID';
+$lang['Unable to perform login'] = 'Anmeldung kann nicht ausgeführt werden';
+$lang['User not found'] = 'Benutzer nicht gefunden';
+$lang['Providers'] = 'Provider';
+$lang['Sign in with %s'] = 'Anmeldung mit %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Entschuldigung, neue Registrierungen sind in dieser Galerie gesperrt.';
+$lang['Or sign in with'] = 'Oder melden Sie sich an mit';
+$lang['Please enter your user ID'] = 'Bitte geben Sie Ihre Benutzer-ID ein';
+$lang['Please enter your username'] = 'Bitte geben Sie Ihren Benutzernamen ein';
+$lang['Microsoft 365 Connect: PHP Curl extension is needed'] = 'Microsoft 365 Verbinden: PHP Curl-Erweiterung wird benötigt';
+$lang['Microsoft 365 Connect: You need to configure the credentials'] = 'Microsoft 365 Connect: Sie müssen die Anmeldeinformationen konfigurieren';
+$lang['Error...'] = 'Fehler...';
+$lang['Loading...'] = 'Laden...';
+$lang['Close'] = 'Schließen';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Kontaktaufnahme mit <b>%s</b>. Bitte warten Sie.';
+$lang['Disabled'] = 'Deaktiviert';
+$lang['Enabled'] = 'Aktiviert';
+$lang['Application Key'] = 'Anwendungsschlüssel';
+$lang['Application Secret'] = 'Anmeldung Geheim';
+$lang['Authentication canceled'] = 'Authentifizierung abgebrochen';
+$lang['Cancel'] = 'Abbruch';
+$lang['%s: invalid keys'] = '%s: ungültige Schlüssel';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Es ist ein Fehler aufgetreten, bitte kontaktieren Sie den Galeristen. <i>Fehlercode : %s</i>';
