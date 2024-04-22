e0025effdcf5f4ab844511f76fc809e08a2e351c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-22 15:19:34 +0200

    Update nb_NO, thanks to paulen

---
+++ language/nb_NO/help.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['And in > Azure Active Directory > Properties, get the Tenant ID'] = 'Og i > Azure Active Directory > Egenskaper får du leietaker-ID';
+$lang['Fill the name and redirect URI :  https://your_piwigo/plugins/m365connect/auth.php'] = 'Fyll ut navnet og omdiriger URI: https://your_piwigo/plugins/m365connect/auth.php';
+$lang['Get the Application (client) ID (= Application Key)'] = 'Få applikasjons (klient) ID (= applikasjonsnøkkel)';
+$lang['Go to <a href="%s" target="_blank">%s</a> > Azure Active Directory > App registrations > Create a tenant'] = 'Gå til <a href="%s" target="_blank">%s</a> > Azure Active Directory > Appregistreringer > Opprett en leietaker';
+$lang['In the tenant configuration, go to > Certificates & secrets. Create a new client secret. (= Application Secret)'] = 'I leietakerkonfigurasjonen går du til > Sertifikater og hemmeligheter. Opprett en ny klienthemmelighet. (= Application Secret)';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Når du har registrert deg, kopier og lim inn den opprettede applikasjonslegitimasjonen til denne oppsettsiden';
---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Tenant ID'] = 'Leietaker ID';
+$lang['Unable to perform login'] = 'Kan ikke utføre pålogging';
+$lang['User not found'] = 'Bruker ikke funnet';
+$lang['%s: invalid keys'] = '%s: ugyldige kode';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Det oppsto en feil. Ta kontakt med gallerieieren. <i>Feilkode: %s</i>';
+$lang['Application Key'] = 'Søknadskode';
+$lang['Application Secret'] = 'Søknadshemmelighet';
+$lang['Authentication canceled'] = 'Autentisering avbrutt';
+$lang['Cancel'] = 'Avbryt';
+$lang['Close'] = 'Lukk';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Kontakter <b>%s</b>. Vennligst vent.';
+$lang['Disabled'] = 'Deaktiver';
+$lang['Enabled'] = 'Aktivert';
+$lang['Error...'] = 'Feil';
+$lang['Loading...'] = 'Laster...';
+$lang['Microsoft 365 Connect: PHP Curl extension is needed'] = 'Microsoft 365 Connect: PHP Curl-utvidelse er nødvendig';
+$lang['Microsoft 365 Connect: You need to configure the credentials'] = 'Microsoft 365 Connect: Du må konfigurere legitimasjonen';
+$lang['Or sign in with'] = 'Eller logg på med';
+$lang['Please enter your user ID'] = 'Vennligst skriv inn din bruker-ID';
+$lang['Please enter your username'] = 'Vennligst skriv inn brukernavnet ditt';
+$lang['Providers'] = 'Leverandører';
+$lang['Sign in with %s'] = 'Logg på med %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Beklager, nye registreringer er blokkert i dette galleriet.';
