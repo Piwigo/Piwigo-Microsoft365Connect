de8c38b938a86e58ad0ec0dd5235dd11e3dc7a38
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-23 09:12:39 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/help.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['And in > Azure Active Directory > Properties, get the Tenant ID'] = 'Y en Azure Active Directory > Propiedades, obtenga el ID de inquilino';
+$lang['Fill the name and redirect URI :  https://your_piwigo/plugins/m365connect/auth.php'] = 'Llene el nombre y el URI de redirección: https://su_piwigo/plugins/m365connect/auth.php';
+$lang['Get the Application (client) ID (= Application Key)'] = 'Obtenga el ID de Aplicación (cliente) (= Application Key)';
+$lang['Go to <a href="%s" target="_blank">%s</a> > Azure Active Directory > App registrations > Create a tenant'] = 'Vaya a <a href="%s" target="_blank">%s</a> > Azure Active Directory > Registro de aplicaciones > Crear un inquilino';
+$lang['In the tenant configuration, go to > Certificates & secrets. Create a new client secret. (= Application Secret)'] = 'En la configuración del inquilino, vaya a > Certificados y secretos, Crear un nuevo secreto de cliente. (= Application Secret)';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Una vez registrada, copie y pegue las credenciales de aplicación creadas en esta página de configuración';
---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s: invalid keys'] = '%s: claves no válidas';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Se ha producido un error; póngase en contacto con el propietario de la galería. <i>Código de error: %s</i>';
+$lang['Application Key'] = 'Clave de aplicación';
+$lang['Application Secret'] = 'Secreto de aplicación';
+$lang['Authentication canceled'] = 'Autenticación cancelada';
+$lang['Cancel'] = 'Cancelar';
+$lang['Close'] = 'Cerrar';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Contactando con <b>%s</b>. Espere por favor.';
+$lang['Disabled'] = 'Inhabilitado';
+$lang['Enabled'] = 'Habilitado';
+$lang['Error...'] = 'Error...';
+$lang['Loading...'] = 'Cargando...';
+$lang['Microsoft 365 Connect: PHP Curl extension is needed'] = 'Microsoft 365 Connect: se necesita la extensión PHP Curl';
+$lang['Microsoft 365 Connect: You need to configure the credentials'] = 'Microsoft 365 Connect: es necesario configurar las credenciales';
+$lang['Or sign in with'] = 'O inicie sesión con';
+$lang['Please enter your user ID'] = 'Introduzca su ID de usuario';
+$lang['Please enter your username'] = 'Introduzca su nombre de usuario';
+$lang['Providers'] = 'Proveedores';
+$lang['Sign in with %s'] = 'Iniciar sesión con %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Lo sentimos, en esta galería se han bloqueado los nuevos registros';
+$lang['Tenant ID'] = 'ID de inquilino';
+$lang['Unable to perform login'] = 'No se puede iniciar sesión';
+$lang['User not found'] = 'Usuario no encontrado';
