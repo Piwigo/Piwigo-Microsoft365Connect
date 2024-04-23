9815b12fb7627dfe205e96af35ea47a08f318f8c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-23 09:12:27 +0200

    Update sk_SK, thanks to dodo

---
+++ language/sk_SK/help.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['And in > Azure Active Directory > Properties, get the Tenant ID'] = 'A v ponuke > Azure Active Directory > Vlastnosti získajte ID nájomcu';
+$lang['Fill the name and redirect URI :  https://your_piwigo/plugins/m365connect/auth.php'] = 'Vyplňte názov a presmerovanie URI : https://your_piwigo/plugins/m365connect/auth.php';
+$lang['Get the Application (client) ID (= Application Key)'] = 'Získanie ID aplikácie (klienta) (= aplikačný kľúč)';
+$lang['Go to <a href="%s" target="_blank">%s</a> > Azure Active Directory > App registrations > Create a tenant'] = 'Prejdite na <a href="%s" target="_blank">%s</a> > Azure Active Directory > Registrácie aplikácií > Vytvoriť nájomcu';
+$lang['In the tenant configuration, go to > Certificates & secrets. Create a new client secret. (= Application Secret)'] = 'V konfigurácii nájomcu prejdite na > Certifikáty a tajomstvá. Vytvorte nové klientské tajomstvo. (= Tajomstvo aplikácie)';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Po registrácii skopírujte a vložte vytvorené prihlasovacie údaje do tejto stránky s nastaveniami.';
---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Microsoft 365 Connect: PHP Curl extension is needed'] = 'Microsoft 365 Connect: Je potrebné rozšírenie PHP Curl';
+$lang['Microsoft 365 Connect: You need to configure the credentials'] = 'Microsoft 365 Connect: Musíte nakonfigurovať poverenia';
+$lang['Or sign in with'] = 'Alebo sa prihláste pomocou';
+$lang['Please enter your user ID'] = 'Zadajte svoje ID používateľa';
+$lang['Please enter your username'] = 'Zadajte svoje používateľské meno';
+$lang['Providers'] = 'Poskytovatelia';
+$lang['Sign in with %s'] = 'Prihláste sa pomocou %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Je nám ľúto, ale nové registrácie sú v tejto galérii zablokované.';
+$lang['Tenant ID'] = 'ID nájomcu';
+$lang['Unable to perform login'] = 'Nie je možné vykonať prihlásenie';
+$lang['User not found'] = 'Používateľ nebol nájdený';
+$lang['%s: invalid keys'] = '%s: neplatné kľúče';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Vyskytla sa chyba, kontaktujte prosím majiteľa galérie. <i>Kód chyby : %s</i>';
+$lang['Application Key'] = 'Kľúč k aplikácii';
+$lang['Application Secret'] = 'Tajná aplikácia';
+$lang['Authentication canceled'] = 'Overovanie zrušené';
+$lang['Cancel'] = 'Zrušiť';
+$lang['Close'] = 'Zatvoriť';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Kontaktovanie <b>%s</b>. Počkajte, prosím.';
+$lang['Disabled'] = 'Zakázané';
+$lang['Enabled'] = 'Povolené';
+$lang['Error...'] = 'Chyba...';
+$lang['Loading...'] = 'Načítavanie...';
