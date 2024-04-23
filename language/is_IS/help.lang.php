c0eb22ce532a3db893071c33ca91b2750ef234d8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-23 11:57:45 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/help.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['And in > Azure Active Directory > Properties, get the Tenant ID'] = 'Og í > Azure Active Directory > Eiginleikar, fáðu leigjanda auðkennið';
+$lang['Fill the name and redirect URI :  https://your_piwigo/plugins/m365connect/auth.php'] = 'Fylltu út nafnið og beina URI:
+https://your_piwigo/plugins/m365connect/auth.php';
+$lang['Get the Application (client) ID (= Application Key)'] = 'Fáðu auðkenni hugbúnaðar (viðskiptavinar) (= lykilorð hugbúnaðar)';
+$lang['Go to <a href="%s" target="_blank">%s</a> > Azure Active Directory > App registrations > Create a tenant'] = 'Farðu í <a href="%s" target="_blank">%s</a> > Azure Active Directory > Innskráning hugbúnaðar > Búa til leigjanda';
+$lang['In the tenant configuration, go to > Certificates & secrets. Create a new client secret. (= Application Secret)'] = 'Í leigjandastillingunni, farðu í > Vottorð og leyniorð. Búðu til nýtt leyniorð viðskiptavinar. (= Leyniorð hugbúnaðar)';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Þegar þú hefur skráð þig skaltu afrita og líma hugbúnaðarskilríkið á þessari uppsetningarsíðu';
---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Microsoft 365 Connect: PHP Curl extension is needed'] = 'Microsoft 365 Connect: PHP Curl viðbót er nauðsynleg';
+$lang['Microsoft 365 Connect: You need to configure the credentials'] = 'Microsoft 365 Connect: Þú þarft að stilla skilríkin';
+$lang['Or sign in with'] = 'Eða skráðu þig inn með';
+$lang['Please enter your user ID'] = 'Vinsamlega sláðu inn notendaauðkennið þitt';
+$lang['Please enter your username'] = 'Vinsamlega sláðu inn notendanafnið þitt';
+$lang['Providers'] = 'Veitendur';
+$lang['Sign in with %s'] = 'Skráðu þig inn með %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Því miður er lokað fyrir nýjar skráningar í þessu myndasafni.';
+$lang['Tenant ID'] = 'Auðkenni leigjanda';
+$lang['Unable to perform login'] = 'Ekki hægt að framkvæma innskráningu';
+$lang['User not found'] = 'Notandi finnst ekki';
+$lang['%s: invalid keys'] = '%s: ógild lykilorð';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Villa kom upp, vinsamlegast hafðu samband við eiganda gallerísins. <i>Villukóði: %s</i>';
+$lang['Application Key'] = 'Lykilorð hugbúnaðar';
+$lang['Application Secret'] = 'Leyniorð hugbúnaðar';
+$lang['Authentication canceled'] = 'Hætt við auðkenningu';
+$lang['Cancel'] = 'Hætta';
+$lang['Close'] = 'Loka';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Hef samband við <b>%s</b>. Vinsamlegast bíðið.';
+$lang['Disabled'] = 'Óvirkjað';
+$lang['Enabled'] = 'Virkjað';
+$lang['Error...'] = 'Villur…';
+$lang['Loading...'] = 'Hleð…';
