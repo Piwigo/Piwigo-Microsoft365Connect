7ed2183d80d25b70b18d038e602ed4fef62190d0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-23 09:12:33 +0200

    Update ru_RU, thanks to evadim

---
+++ language/ru_RU/help.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'После регистрации скопируйте и вставьте созданные учетные данные приложения на эту страницу настройки.';
---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Извините, регистрация в этой галерее отключена.';
+$lang['Or sign in with'] = 'Или войдите с';
+$lang['Sign in with %s'] = 'Войдите с %s';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Произошла ошибка, свяжитесь с владельцем галереи. <i>Код ошибки: %s</i>';
+$lang['Authentication canceled'] = 'Аутентификация отменена';
+$lang['Unable to perform login'] = 'Невозможно выполнить вход';
+$lang['User not found'] = 'Пользователь не найден';
+$lang['Cancel'] = 'Отменить';
+$lang['Close'] = 'Закрыть';
+$lang['Disabled'] = 'Выключено';
+$lang['Enabled'] = 'Включено';
+$lang['Error...'] = 'Ошибка...';
+$lang['Loading...'] = 'Загрузка...';
