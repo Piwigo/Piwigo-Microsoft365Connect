331c306791676370e5e79d318427045804149955
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-23 09:12:36 +0200

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/help.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Fill the name and redirect URI :  https://your_piwigo/plugins/m365connect/auth.php'] = 'Ompliu el nom i l\'URI de redirecció: https://your_piwigo/plugins/m365connect/auth.php';
+$lang['Get the Application (client) ID (= Application Key)'] = 'Obteniu l\'identificador de l\'aplicació (client) (= Clau de l\'aplicació)';
+$lang['Go to <a href="%s" target="_blank">%s</a> > Azure Active Directory > App registrations > Create a tenant'] = 'Aneu a <a href="%s" target="_blank">%s</a> > Azure Active Directory > Registres d\'aplicacions > Crea un llogater';
+$lang['In the tenant configuration, go to > Certificates & secrets. Create a new client secret. (= Application Secret)'] = 'A la configuració del llogater aneu a > Certificats i secrets. Creeu un nou secret de client. (= Secret de l\'aplicació)';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Un cop us hàgiu registrat, copieu i enganxeu les credencials de l\'aplicació creada en aquesta pàgina de configuració';
+$lang['And in > Azure Active Directory > Properties, get the Tenant ID'] = 'I a > Azure Active Directory > Propietats, obteniu l\'ID de llogater';
---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Tenant ID'] = 'ID de llogater';
+$lang['%s: invalid keys'] = '%s: claus no vàlides';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'S\'ha produït un error, poseu-vos en contacte amb el propietari de la galeria. <i>Codi d\'error: %s</i>';
+$lang['Application Key'] = 'Clau d\'aplicació';
+$lang['Application Secret'] = 'Secret d\'aplicació';
+$lang['Authentication canceled'] = 'Autenticació cancel·lada';
+$lang['Cancel'] = 'Cancel·la';
+$lang['Close'] = 'Tanca';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'S\'està contactant amb <b>%s</b>. Espereu, si us plau.';
+$lang['Disabled'] = 'Inhabilitat';
+$lang['Enabled'] = 'Habilitat';
+$lang['Error...'] = 'Error...';
+$lang['Loading...'] = 'Carregant...';
+$lang['Microsoft 365 Connect: PHP Curl extension is needed'] = 'Microsoft 365 Connect: es necessita l\'extensió PHP Curl';
+$lang['Microsoft 365 Connect: You need to configure the credentials'] = 'Microsoft 365 Connect: és necessari configurar les credencials';
+$lang['Or sign in with'] = 'O inicieu sessió amb';
+$lang['Please enter your user ID'] = 'Introduïu el vostre ID d\'usuari';
+$lang['Please enter your username'] = 'Introduïu el vostre nom d\'usuari';
+$lang['Providers'] = 'Proveïdors';
+$lang['Sign in with %s'] = 'Inicieu sessió amb %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Ens sap greu, els nous registres estan bloquejats en aquesta galeria.';
+$lang['Unable to perform login'] = 'No es pot iniciar sessió';
+$lang['User not found'] = 'Usuari no trobat';
