ead6c10c5b4f372994a2c2530220000554b643c7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-23 09:12:42 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/help.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['And in > Azure Active Directory > Properties, get the Tenant ID'] = 'Ve > Azure Active Directory > Özellikler bölümünde Kiracı Kimliğini alın';
+$lang['Fill the name and redirect URI :  https://your_piwigo/plugins/m365connect/auth.php'] = 'Adı girin ve URI\'yi yönlendirin: https://your_piwigo/plugins/m365connect/auth.php';
+$lang['Go to <a href="%s" target="_blank">%s</a> > Azure Active Directory > App registrations > Create a tenant'] = '<a href="%s" target="_blank">%s</a> > Azure Active Directory > Uygulama kayıtları > Kiracı oluştur\'a gidin';
+$lang['Get the Application (client) ID (= Application Key)'] = 'Uygulama (istemci) kimliğini (= Uygulama Anahtarı) alın';
+$lang['In the tenant configuration, go to > Certificates & secrets. Create a new client secret. (= Application Secret)'] = 'Kiracı yapılandırmasında > Sertifikalar ve Sırlar\'a gidin. Yeni bir istemci sırrı oluşturun. (= Uygulama Sırrı)';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Kaydolduktan sonra, oluşturulan uygulama kimlik bilgilerini kopyalayıp bu kurulum sayfasına yapıştırın';
---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Application Secret'] = 'Uygulama Sırrı';
+$lang['Tenant ID'] = 'Kiracı kimliği';
+$lang['Unable to perform login'] = 'Oturum açma işlemi gerçekleştirilemiyor';
+$lang['Cancel'] = 'Vazgeç';
+$lang['Or sign in with'] = 'ya da bununla giriş yapın';
+$lang['Providers'] = 'Sağlayıcılar';
+$lang['User not found'] = 'Kullanıcı bulunamadı';
+$lang['Disabled'] = 'Devre dışı';
+$lang['Contacting <b>%s</b>. Please wait.'] = '<b>%s</b>\'ye bağlanılıyor. Lütfen bekleyin.';
+$lang['Close'] = 'Kapat';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Bir hata oluştu. Lütfen galeri sahibiyle iletişime geçin. <i>Hata kodu: %s</i>';
+$lang['%s: invalid keys'] = '%s: geçersiz anahtarlar';
+$lang['Authentication canceled'] = 'Kimlik doğrulama iptal edildi';
+$lang['Application Key'] = 'Uygulama Anahtarı';
+$lang['Loading...'] = 'Yükleniyor...';
+$lang['Error...'] = 'Hata...';
+$lang['Enabled'] = 'Etkin';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Üzgünüz, bu galeride yeni kayıtlar engellendi.';
+$lang['Sign in with %s'] = '%s ile oturum açın';
+$lang['Please enter your username'] = 'Lütfen kullanıcı adınızı girin';
+$lang['Please enter your user ID'] = 'Lütfen kullanıcı kimliğinizi girin';
+$lang['Microsoft 365 Connect: You need to configure the credentials'] = 'Microsoft 365 Connect: Kimlik bilgilerini yapılandırmanız gerekiyor';
+$lang['Microsoft 365 Connect: PHP Curl extension is needed'] = 'Microsoft 365 Connect: PHP Curl uzantısı gerekli';
