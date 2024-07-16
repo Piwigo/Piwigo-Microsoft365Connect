fad8c5fb2c22ca8b0051d3aa792ba3f6feec3f3d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-16 09:09:15 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Helaas, nieuwe registraties in deze galerij zijn geblokkeerd.';
+$lang['Sign in with %s'] = 'Meld je aan met %s';
+$lang['Providers'] = 'Leveranciers';
+$lang['Please enter your username'] = 'Voer uw gebruikersnaam in';
+$lang['Please enter your user ID'] = 'Voer uw gebruikers-ID in';
+$lang['Or sign in with'] = 'Of meld u aan met';
+$lang['Microsoft 365 Connect: You need to configure the credentials'] = 'Microsoft 365 Connect: U moet de referenties configureren';
+$lang['Microsoft 365 Connect: PHP Curl extension is needed'] = 'Microsoft 365 Connect: PHP Curl-extensie is nodig';
+$lang['Loading...'] = 'Bezig met laden...';
+$lang['Error...'] = 'Fout...';
+$lang['Enabled'] = 'Ingeschakeld';
+$lang['Disabled'] = 'Gehandicapt';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Contact opnemen met <b>%s</b>. Even geduld.';
+$lang['Close'] = 'Dichtbij';
+$lang['Cancel'] = 'Annuleren';
+$lang['Authentication canceled'] = 'Authenticatie geannuleerd';
+$lang['Application Secret'] = 'Toepassingsgeheim';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Er is een fout opgetreden. Neem contact op met de galerie-eigenaar. <i>Foutcode: %s</i>';
+$lang['Application Key'] = 'Toepassingssleutel';
