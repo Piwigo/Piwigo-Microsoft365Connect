e790e11c2e2f68acbc1258e7f2e87f8e0e40e8b6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-04 10:47:43 +0200

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/help.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['And in > Azure Active Directory > Properties, get the Tenant ID'] = '然後 > Azure Active Directory > 內容，取得 Tenant ID';
+$lang['Fill the name and redirect URI :  https://your_piwigo/plugins/m365connect/auth.php'] = '填入名稱和轉向的網址：https://your_piwigo/plugins/m365connect/auth.php';
+$lang['Get the Application (client) ID (= Application Key)'] = '取得應用程式（客戶端程式）ID（= 應用程式金鑰）';
+$lang['Go to <a href="%s" target="_blank">%s</a> > Azure Active Directory > App registrations > Create a tenant'] = '前往 <a href="%s" target="_blank">%s</a> > Azure Active Directory > App 註冊 > 建立 tenant';
+$lang['In the tenant configuration, go to > Certificates & secrets. Create a new client secret. (= Application Secret)'] = '在 tenant 組態中，前往 > Certificates & secrets。建立新的客戶端密鑰。（= 應用程式密鑰）';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = '當您完成註冊程序後，複製所產生的應用程式憑證，然後貼到這個設定頁面中';
---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Tenant ID'] = 'Tenant ID';
+$lang['Unable to perform login'] = '無法登入';
+$lang['User not found'] = '找不到使用者';
+$lang['Disabled'] = '禁用';
+$lang['Enabled'] = '啟用';
+$lang['Error...'] = '錯誤...';
+$lang['Loading...'] = '載入中...';
+$lang['Microsoft 365 Connect: PHP Curl extension is needed'] = 'Microsoft 365 Connect: 需要 PHP Curl 延伸套件';
+$lang['Microsoft 365 Connect: You need to configure the credentials'] = 'Microsoft 365 Connect: 您需要設定憑證組態';
+$lang['Or sign in with'] = '或是用以下方式登入';
+$lang['Please enter your user ID'] = '請輸入您的使用者 ID';
+$lang['Please enter your username'] = '請輸入您的使用者名稱';
+$lang['Providers'] = '提供者';
+$lang['Sign in with %s'] = '使用 %s 登入';
+$lang['Sorry, new registrations are blocked on this gallery.'] = '抱歉，此網站不允許新的註冊。';
+$lang['Contacting <b>%s</b>. Please wait.'] = '正在聯絡 <b>%s</b>。請稍等。';
+$lang['Cancel'] = '取消';
+$lang['Authentication canceled'] = '已取消授權';
+$lang['Application Secret'] = '應用程式密鑰';
+$lang['Application Key'] = '應用程式金錀';
+$lang['Close'] = '關閉';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = '發生錯誤，請聯絡網站管理者。<i>錯誤代碼：%s</i>';
+$lang['%s: invalid keys'] = '%s：不合法的金鑰';
