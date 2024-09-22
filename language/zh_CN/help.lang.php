421a00a18587758b5e231f968367ab4175ea32f5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-22 07:22:44 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/help.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['And in > Azure Active Directory > Properties, get the Tenant ID'] = '在> Azure Active Directory > 属性中，获取租户ID';
+$lang['Fill the name and redirect URI :  https://your_piwigo/plugins/m365connect/auth.php'] = '填写名称并重定向URI：https://your_piwigo/plugins/m365connect/auth.php';
+$lang['Get the Application (client) ID (= Application Key)'] = '获取应用程序（客户端）ID（=应用程序密钥）';
+$lang['Go to <a href="%s" target="_blank">%s</a> > Azure Active Directory > App registrations > Create a tenant'] = '转到 <a href="%s" target="_blank">%s</a> > Azure Active Directory > 应用注册 > 创建租户';
+$lang['In the tenant configuration, go to > Certificates & secrets. Create a new client secret. (= Application Secret)'] = '在租户配置中，转到 > 证书和机密。创建一个新的客户端机密。（= 应用程序机密）';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = '注册后，将创建的应用程序凭据复制并粘贴到此设置页面';
---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Disabled'] = '已禁用';
+$lang['Enabled'] = '已启用';
+$lang['Error...'] = '错误...';
+$lang['Loading...'] = '加载中...';
+$lang['Or sign in with'] = '或使用以下方式登录';
+$lang['Please enter your user ID'] = '请输入您的用户ID';
+$lang['Please enter your username'] = '请输入您的用户名';
+$lang['Providers'] = '提供者';
+$lang['Sign in with %s'] = '使用 %s 登录';
+$lang['Sorry, new registrations are blocked on this gallery.'] = '抱歉，该图库已阻止新注册。';
+$lang['Tenant ID'] = '租户ID';
+$lang['Unable to perform login'] = '无法登录';
+$lang['User not found'] = '未找到用户';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = '出现错误，请联系图库所有者。<i>错误代码：%s</i>';
+$lang['Application Key'] = '应用程序密钥';
+$lang['Application Secret'] = '应用程序密钥';
+$lang['Authentication canceled'] = '认证取消';
+$lang['Cancel'] = '取消';
+$lang['Close'] = '关闭';
+$lang['Contacting <b>%s</b>. Please wait.'] = '正在联系<b>%s</b>。请稍候。';
+$lang['Microsoft 365 Connect: PHP Curl extension is needed'] = 'Microsoft 365 Connect：需要PHP Curl扩展';
+$lang['Microsoft 365 Connect: You need to configure the credentials'] = 'Microsoft 365 Connect：您需要配置凭据';
+$lang['%s: invalid keys'] = '%s:无效密钥';
