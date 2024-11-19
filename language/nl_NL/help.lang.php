7522d2edca4ba8c16e69cd7bb0a3aaf6ab1b4526
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-19 06:53:24 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/help.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Zodra u zich hebt geregistreerd, kopieert en plakt u de aangemaakte applicatiegegevens op deze instellingenpagina';
+$lang['In the tenant configuration, go to > Certificates & secrets. Create a new client secret. (= Application Secret)'] = 'Ga in de tenantconfiguratie naar > Certificaten en geheimen. Maak een nieuw clientgeheim. (= Toepassingsgeheim)';
+$lang['Go to <a href="%s" target="_blank">%s</a> > Azure Active Directory > App registrations > Create a tenant'] = 'Haal de toepassings-ID (client-ID) op (= toepassingssleutel)Ga naar <a href="%s" target="_blank">%s</a> > Azure Active Directory > App-registraties > Een tenant maken';
+$lang['Get the Application (client) ID (= Application Key)'] = 'De applicatie-ID (client-ID) ophalen (= applicatiesleutel)';
+$lang['Fill the name and redirect URI :  https://your_piwigo/plugins/m365connect/auth.php'] = 'Vul de naam en de omleidings-URI in: https://your_piwigo/plugins/m365connect/auth.php';
+$lang['And in > Azure Active Directory > Properties, get the Tenant ID'] = 'En in > Azure Active Directory > Eigenschappen, de Tenant-ID ophalen';
