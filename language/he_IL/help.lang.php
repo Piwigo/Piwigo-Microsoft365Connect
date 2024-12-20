9ccb7f32d1ae8a18fb63872ac9159ca85b3c1aff
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:19:30 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/help.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['And in > Azure Active Directory > Properties, get the Tenant ID'] = 'וב > Azure Active Directory > Properties, קבל את מזהה השוכר';
+$lang['Fill the name and redirect URI :  https://your_piwigo/plugins/m365connect/auth.php'] = 'מלא את השם וכתובת ההפניה: https://your_piwigo/plugins/m365connect/auth.php';
+$lang['Get the Application (client) ID (= Application Key)'] = 'קבל את מזהה האפליקציה (לקוח) (= מפתח האפליקציה)';
+$lang['Go to <a href="%s" target="_blank">%s</a> > Azure Active Directory > App registrations > Create a tenant'] = 'עבור אל <a href="%s" target="_blank">%s</a> > Azure Active Directory > App registrations > צור שוכר';
+$lang['In the tenant configuration, go to > Certificates & secrets. Create a new client secret. (= Application Secret)'] = 'בתצורת השוכר, עבור אל > Certificates & secrets. צור סוד לקוח חדש. (= סוד האפליקציה)';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'לאחר שנרשמת, העתק והדבק את פרטי האפליקציה שנוצרו לעמוד ההתקנה הזה';
---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s: invalid keys'] = '%s: מפתחות לא תקפים';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'שגיאה התרחשה, אנא צור קשר עם בעל הגלריה. <i>קוד שגיאה: %s</i>';
+$lang['Application Key'] = 'מפתח אפליקציה';
+$lang['Application Secret'] = 'סוד אפליקציה';
+$lang['Authentication canceled'] = 'האימות בוטל';
+$lang['Cancel'] = 'ביטול';
+$lang['Close'] = 'סגור';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'מתקשר<b>%s</b>. אנא המתן.';
+$lang['Disabled'] = 'מנוטרל';
+$lang['Enabled'] = 'מופעל';
+$lang['Error...'] = 'שגיאה...';
+$lang['Loading...'] = 'טוען...';
+$lang['Microsoft 365 Connect: PHP Curl extension is needed'] = 'Microsoft 365 Connect: נדרשת תוספת PHP Curl';
+$lang['Microsoft 365 Connect: You need to configure the credentials'] = 'Microsoft 365 Connect: עליך להגדיר את האישורים';
+$lang['Or sign in with'] = 'או התחבר עם';
+$lang['Please enter your user ID'] = 'אנא הזן את מזהה המשתמש שלך';
+$lang['Please enter your username'] = 'אנא הזן את שם המשתמש שלך';
+$lang['Providers'] = 'ספקים';
+$lang['Sign in with %s'] = 'התחבר עם %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'מצטערים, ההרשמות החדשות נחסמות בגלריה זו.';
+$lang['Tenant ID'] = 'מזהה נשכר';
+$lang['Unable to perform login'] = 'לא ניתן לבצע התחברות';
+$lang['User not found'] = 'משתמש לא נמצא';
